ec23f0b40eaaf37dd44bda7a7dc203518663c319
LucasKim89 <lucas.test.c9@gmail.com>
2023-03-30 10:31:06 +1300

    add sample test to check

---
+++ SeleniumFramework/Tests/Test01.cs
@@ -0,0 +1,19 @@
+﻿using SeleniumFrameworkTests.pageObjects;
+using SeleniumFrameworkTests.utilities;
+
+namespace SeleniumFrameworkTests.Tests
+{
+    [Order(4)]
+    public class Test01 : Base
+    {
+        [Order(1)]
+        public void Test01Test()
+        {
+            getDriver();
+
+            driver.Value.Navigate().GoToUrl("https://google.com/");
+
+            Assert.Pass();
+        }
+    }
+}
